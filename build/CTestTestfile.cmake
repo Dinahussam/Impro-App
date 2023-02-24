ede848a732755e5b4c272fe76a7de419ea7fa7f4
MohamedAIsmail <mohamedaismail214@gmail.com>
2023-02-24 10:40:47 +0200

    Revert "Noise, To gray Functions"
    This reverts commit 9142f4806f5c43a6544d606b97659b22ae3cb3a7.

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Anwar/Desktop/SBME 2024/YEAR 3 (2022-2023)/SEMESTER 2/Computer Vision/Tasks/Task 1/CV_task1_team15
-# Build directory: C:/Users/Anwar/Desktop/SBME 2024/YEAR 3 (2022-2023)/SEMESTER 2/Computer Vision/Tasks/Task 1/CV_task1_team15/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
