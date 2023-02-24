9142f4806f5c43a6544d606b97659b22ae3cb3a7
omaranwar21 <omarahmed11185@gmail.com>
2023-02-24 02:44:56 +0200

    Noise, To gray Functions
    Gaussian Noise, uniform noise
    adjust threshold: helper function for add uniform noise main function
    To gray function
    Notes: make sure of the correctness of the path of the picture used.
    File noise.cpp is for functions related to the noise implemented functions "well be reordered"

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Anwar/Desktop/SBME 2024/YEAR 3 (2022-2023)/SEMESTER 2/Computer Vision/Tasks/Task 1/CV_task1_team15
+# Build directory: C:/Users/Anwar/Desktop/SBME 2024/YEAR 3 (2022-2023)/SEMESTER 2/Computer Vision/Tasks/Task 1/CV_task1_team15/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
