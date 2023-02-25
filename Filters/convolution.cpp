755d201d11e64d1b35194bcc4860e29e45f348be
Omar-Saad-ELGharbawy <84602951+Omar-Saad-ELGharbawy@users.noreply.github.com>
2023-02-25 14:55:29 +0200

    transform kernel function

--- Filters/convolution.cpp
+++ Filters/convolution.cpp
@@ -5,6 +5,20 @@
 using namespace cv;
 using namespace std;
 
+Mat transform_kernel(const Mat &src){
+
+    Mat transformed_kernel = Mat::zeros(Size(src.rows,src.cols),src.type());
+    for (int i = 0 ; i<src.rows ; i++)
+    {
+        for (int j = 0 ; j<src.cols ; j++)
+        {
+            transformed_kernel.at<float>(i,j) = src.at<float>(src.rows-1-i,src.cols-1-j);
+        }
+    }
+    return transformed_kernel;
+}
+
+
 void Padd_Image(const Mat &src){
     Mat padding_img = Mat::zeros(Size(src.rows+2,src.cols+2),src.type());
 
