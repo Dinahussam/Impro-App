12d5bb1d565c825c2995632471900b95a27d608a
Omar-Saad-ELGharbawy <84602951+Omar-Saad-ELGharbawy@users.noreply.github.com>
2023-02-25 17:36:49 +0200

    convolute mono

--- Filters/convolution.cpp
+++ Filters/convolution.cpp
@@ -54,3 +54,31 @@ Mat Padd_RGB(const Mat &src){
     }
     return padding_img;
 }
+
+Mat convolute_mono(const Mat &src , const Mat &kernel)
+{
+    Mat padded_img = Padd_Mono(src);
+
+    Mat new_kernel =  transform_kernel(kernel);
+
+    Mat convoluted_img = Mat::zeros(Size(src.rows,src.cols),src.type());
+
+    float conv_sum;
+// convolute image
+    for (int i =1;i<padded_img.rows-1 ; i++)
+    {
+        for (int j = 1 ; j<padded_img.cols-1 ; j++)
+        {
+            conv_sum = 0;
+            for(int m = 0 ; m<3;m++ )
+            {
+                for(int n = 0 ; n<3 ; n++)
+                {
+                    conv_sum += padded_img.at<uchar>(i+m-1,j+n-1) * new_kernel.at<float>(m,n);
+                }
+            }
+            convoluted_img.at<uchar>(i-1,j-1) = (int)conv_sum;
+        }
+    }
+    return convoluted_img;
+}
