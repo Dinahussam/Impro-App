3edd128cce0d2e21348e3731587f8b6374200f1f
Omar-Saad-ELGharbawy <84602951+Omar-Saad-ELGharbawy@users.noreply.github.com>
2023-02-25 17:33:24 +0200

    padding mono and rgb

--- Filters/convolution.cpp
+++ Filters/convolution.cpp
@@ -19,9 +19,27 @@ Mat transform_kernel(const Mat &src){
 }
 
 
-Mat Padd_Image(const Mat &src){
+Mat Padd_Mono(const Mat &src){
     Mat padding_img = Mat::zeros(Size(src.rows+2,src.cols+2),src.type());
+    // Padding ----------------------------------------------------
+    for( int i=0; i<src.rows; i++)
+    {
+        for(int j=0; j<src.cols; j++)
+        {
+            // copy rgb to rgb
+            // for(int c=0; c<3; c++)
+            // {
+                // padding_img.at<Vec3b>(i+1,j+1)[c] = src.at<Vec3b>(i,j)[c];
+            padding_img.at<uchar>(i+1,j+1) = src.at<uchar>(i,j);
 
+            // }
+        }
+    }
+    return padding_img;
+}
+
+Mat Padd_RGB(const Mat &src){
+    Mat padding_img = Mat::zeros(Size(src.rows+2,src.cols+2),src.type());
     // Padding ----------------------------------------------------
     for( int i=0; i<src.rows; i++)
     {
