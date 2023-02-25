10b4778576af55ebb6b8b0d81c8ff1057b6c667c
Omar-Saad-ELGharbawy <84602951+Omar-Saad-ELGharbawy@users.noreply.github.com>
2023-02-26 00:54:33 +0200

    rgb convolution

--- Filters/convolution.cpp
+++ Filters/convolution.cpp
@@ -7,7 +7,8 @@ using namespace std;
 
 Mat transform_kernel(const Mat &src){
 
-    Mat transformed_kernel = Mat::zeros(Size(src.rows,src.cols),src.type());
+    Mat transformed_kernel = Mat::zeros(Size(src.cols,src.rows),src.type());
+    // cout <<src.type() << endl;
     for (int i = 0 ; i<src.rows ; i++)
     {
         for (int j = 0 ; j<src.cols ; j++)
@@ -18,9 +19,9 @@ Mat transform_kernel(const Mat &src){
     return transformed_kernel;
 }
 
-
 Mat Padd_Mono(const Mat &src){
-    Mat padding_img = Mat::zeros(Size(src.rows+2,src.cols+2),src.type());
+    Mat padding_img = Mat::zeros(Size(src.cols+2,src.rows+2),src.type());
+
     // Padding ----------------------------------------------------
     for( int i=0; i<src.rows; i++)
     {
@@ -39,7 +40,9 @@ Mat Padd_Mono(const Mat &src){
 }
 
 Mat Padd_RGB(const Mat &src){
-    Mat padding_img = Mat::zeros(Size(src.rows+2,src.cols+2),src.type());
+    Mat padding_img = Mat::zeros(Size(src.cols+2,src.rows+2),src.type());
+    cout<< "Padded size: " << padding_img.size() << endl;
+
     // Padding ----------------------------------------------------
     for( int i=0; i<src.rows; i++)
     {
@@ -55,13 +58,14 @@ Mat Padd_RGB(const Mat &src){
     return padding_img;
 }
 
-Mat convolute_mono(const Mat &src , const Mat &kernel)
+
+Mat Convolute_2d_Mono(const Mat &src , const Mat &kernel)
 {
     Mat padded_img = Padd_Mono(src);
 
     Mat new_kernel =  transform_kernel(kernel);
 
-    Mat convoluted_img = Mat::zeros(Size(src.rows,src.cols),src.type());
+    Mat convoluted_img = Mat::zeros(Size(src.cols,src.rows),src.type());
 
     float conv_sum;
 // convolute image
@@ -80,6 +84,45 @@ Mat convolute_mono(const Mat &src , const Mat &kernel)
             // absolute of conv_sum
             conv_sum = abs(conv_sum);
             convoluted_img.at<uchar>(i-1,j-1) = (int)conv_sum;
+        }
+    }
+    return convoluted_img;
+}
+
+Mat Convolute_2d_RGB(const Mat &src , const Mat &kernel)
+{
+    Mat padded_img = Padd_RGB(src);
+
+    Mat new_kernel =  transform_kernel(kernel);
+
+    Mat convoluted_img = Mat::zeros(Size(src.cols,src.rows),src.type());
+
+    float conv_sum_r,conv_sum_g,conv_sum_b;
+// convolute image
+    for (int i =1;i<padded_img.rows-1 ; i++)
+    {
+        for (int j = 1 ; j<padded_img.cols-1 ; j++)
+        {
+            conv_sum_r = 0;
+            conv_sum_g = 0;
+            conv_sum_b = 0;
+            for(int m = 0 ; m<3;m++ )
+            {
+                for(int n = 0 ; n<3 ; n++)
+                {
+                    conv_sum_r += padded_img.at<Vec3b>(i+m-1,j+n-1)[0] * new_kernel.at<float>(m,n);
+                    conv_sum_g += padded_img.at<Vec3b>(i+m-1,j+n-1)[1] * new_kernel.at<float>(m,n);
+                    conv_sum_b += padded_img.at<Vec3b>(i+m-1,j+n-1)[2] * new_kernel.at<float>(m,n);
+                }
+            }
+            // absolute of conv_sum
+            conv_sum_r = abs(conv_sum_r);
+            conv_sum_g = abs(conv_sum_g);
+            conv_sum_b = abs(conv_sum_b);
+
+            convoluted_img.at<Vec3b>(i-1,j-1)[0] = (int)conv_sum_r;
+            convoluted_img.at<Vec3b>(i-1,j-1)[1] = (int)conv_sum_g;
+            convoluted_img.at<Vec3b>(i-1,j-1)[2] = (int)conv_sum_b;
         }
     }
     return convoluted_img;
