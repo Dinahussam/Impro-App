20b85eeff4035244271efbc1af3fab88dd854bb9
MohamedAIsmail <mohamedaismail214@gmail.com>
2023-03-08 22:36:29 +0200

    GUI Updates w/ including hybrid functions

--- QT_GUI_TASK_1/mainwindow.cpp
+++ QT_GUI_TASK_1/mainwindow.cpp
@@ -8,14 +8,11 @@
 #include "Filters/prewitt.hpp"
 #include "Filters/robert.hpp"
 #include "Filters/sobel.hpp"
+#include "Frequency/frequency.hpp"
 #include "Threshold/Thresholding.hpp"
 #include "imageClass.hpp"
 
 
-#include <opencv2/opencv.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgproc.hpp>
-#include "opencv2/core/core.hpp"
 #include <QFileDialog>
 #include <QImageReader>
 #include <QImage>
@@ -26,12 +23,6 @@
 using namespace cv;
 using namespace std;
 
-QImage inputImage;
-Mat inputMat = Mat::zeros(1, 1, CV_64F);
-
-Mat filterOutputMat = Mat::zeros(1, 1, CV_64F);
-Mat edgeDetectionOutputMat = Mat::zeros(1, 1, CV_64F);
-Mat thresholdOutputMat = Mat::zeros(1, 1, CV_64F);
 
 
 MainWindow::MainWindow(QWidget *parent) :
@@ -39,47 +30,43 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->pushButton->setDisabled(true);
 
 }
+
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
 
+
 void MainWindow::on_BrowseButton_clicked()
 {
-    QImageReader reader;
+    UploadImage(inputImage, inputMat);
 
-        reader.setFileName(QFileDialog::getOpenFileName(this,tr("Open image")));
-        inputImage= reader.read();
-
-        if(checkImage()) return;
-
-        inputImage = inputImage.convertToFormat(QImage::Format_BGR888);
-        inputMat = Mat(inputImage.height(), inputImage.width(), CV_8UC3, inputImage.bits(), inputImage.bytesPerLine());
-
-        Convert_To_Gray(inputMat, filterOutputMat);
-        Convert_To_Gray(inputMat, edgeDetectionOutputMat);
+    Convert_To_Gray(inputMat, filterOutputMat);
+    Convert_To_Gray(inputMat, edgeDetectionOutputMat);
 
 
-        ui->filter_inputImage->setPixmap(QPixmap::fromImage(QImage(inputMat.data, inputMat.cols, inputMat.rows, inputMat.step, QImage::Format_BGR888)));
-        ui->EdgeDetection_inputImage->setPixmap(QPixmap::fromImage(QImage(inputMat.data, inputMat.cols, inputMat.rows, inputMat.step, QImage::Format_BGR888)));
-        ui->Threshold_InputImage->setPixmap(QPixmap::fromImage(QImage(inputMat.data, inputMat.cols, inputMat.rows, inputMat.step, QImage::Format_BGR888)));
-
-
-        ui->filter_outputImage->setPixmap(QPixmap::fromImage(QImage(filterOutputMat.data, filterOutputMat.cols, filterOutputMat.rows, filterOutputMat.step, QImage::Format_Grayscale8)));
-
+    updateImage(inputMat, ui->filter_inputImage, 1);
+    updateImage(inputMat, ui->EdgeDetection_inputImage, 1);
+    updateImage(inputMat, ui->Threshold_InputImage, 1);
+    updateImage(filterOutputMat, ui->filter_outputImage, 0);
 }
 
 
+
+
+// ----------------------------------------------------------- FILTERING & NOISE TAB -----------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------------------------------
+
 void MainWindow::on_MedianFilterButton_clicked()
 {
-    if(checkImage()) return;
-
+    if(checkImage(inputImage)) return;
 
     Add_Median_Filter(filterOutputMat, filterOutputMat);
-    ui->filter_outputImage->setPixmap(QPixmap::fromImage(QImage(filterOutputMat.data, filterOutputMat.cols, filterOutputMat.rows, filterOutputMat.step, QImage::Format_Grayscale8)));
+    updateImage(filterOutputMat, ui->filter_outputImage, 0);
 
 }
 
@@ -87,36 +74,35 @@ void MainWindow::on_MedianFilterButton_clicked()
 
 void MainWindow::on_AverageFilterButton_clicked()
 {
-    if(checkImage()) return;
+    if(checkImage(inputImage)) return;
 
     Add_Average_Filter(filterOutputMat, filterOutputMat);
-    ui->filter_outputImage->setPixmap(QPixmap::fromImage(QImage(filterOutputMat.data, filterOutputMat.cols, filterOutputMat.rows, filterOutputMat.step, QImage::Format_Grayscale8)));
+    updateImage(filterOutputMat, ui->filter_outputImage, 0);
 }
 
 
 void MainWindow::on_GrayScale_clicked()
 {
-    if(checkImage()) return;
+    if(checkImage(inputImage)) return;
 
     cv::resize(inputMat, inputMat, cv::Size(512,512),0,0);
     Convert_To_Gray(inputMat, filterOutputMat);
-    ui->filter_outputImage->setPixmap(QPixmap::fromImage(QImage(filterOutputMat.data, filterOutputMat.cols, filterOutputMat.rows, filterOutputMat.step, QImage::Format_Grayscale8)));
-
+    updateImage(filterOutputMat, ui->filter_outputImage, 0);
 }
 
 
 void MainWindow::on_GaussianFilterButton_clicked()
 {
-    if(checkImage()) return;
+    if(checkImage(inputImage)) return;
 
     filterOutputMat = Gaussian_Filter(filterOutputMat);
-    ui->filter_outputImage->setPixmap(QPixmap::fromImage(QImage(filterOutputMat.data, filterOutputMat.cols, filterOutputMat.rows, filterOutputMat.step, QImage::Format_Grayscale8)));
+    updateImage(filterOutputMat, ui->filter_outputImage, 0);
 }
 
 
 void MainWindow::on_SaltPepperNoiseButton_clicked()
 {
-    if(checkImage()) return;
+    if(checkImage(inputImage)) return;
 
     SaltPepperParameters SaltPepperParameter;
     SaltPepperParameter.setModal(true);
@@ -125,14 +111,13 @@ void MainWindow::on_SaltPepperNoiseButton_clicked()
     if(SaltPepperParameter.flag) return;
 
     Add_Salt_And_Pepper_Noise(filterOutputMat, filterOutputMat, SaltPepperParameter.saltAndPepperAmount);
-    ui->filter_outputImage->setPixmap(QPixmap::fromImage(QImage(filterOutputMat.data, filterOutputMat.cols, filterOutputMat.rows, filterOutputMat.step, QImage::Format_Grayscale8)));
-
+    updateImage(filterOutputMat, ui->filter_outputImage, 0);
 }
 
 
 void MainWindow::on_GaussianNoiseButton_clicked()
 {
-    if(checkImage()) return;
+    if(checkImage(inputImage)) return;
 
     GaussianNoiseParameters gaussianParameters;
     gaussianParameters.setModal(true);
@@ -141,12 +126,12 @@ void MainWindow::on_GaussianNoiseButton_clicked()
     if(gaussianParameters.flag) return;
 
     Add_Gaussian_Noise(filterOutputMat, filterOutputMat, gaussianParameters.gaussianMeanValue, gaussianParameters.gaussianSTDValue, gaussianParameters.noiseIntenisty);
-    ui->filter_outputImage->setPixmap(QPixmap::fromImage(QImage(filterOutputMat.data, filterOutputMat.cols, filterOutputMat.rows, filterOutputMat.step, QImage::Format_Grayscale8)));
+    updateImage(filterOutputMat, ui->filter_outputImage, 0);
 }
 
 void MainWindow::on_UniformNoiseButton_clicked()
 {
-    if(checkImage()) return;
+    if(checkImage(inputImage)) return;
 
     UniformNoiseParameters uniformParameters;
     uniformParameters.setModal(true);
@@ -155,62 +140,71 @@ void MainWindow::on_UniformNoiseButton_clicked()
     if(uniformParameters.flag) return;
 
     Add_Uniform_Noise(filterOutputMat, filterOutputMat, uniformParameters.noiseIntenisty);
-    ui->filter_outputImage->setPixmap(QPixmap::fromImage(QImage(filterOutputMat.data, filterOutputMat.cols, filterOutputMat.rows, filterOutputMat.step, QImage::Format_Grayscale8)));
+    updateImage(filterOutputMat, ui->filter_outputImage, 0);
 }
 
+
+
+
+
+// ----------------------------------------------------------- EDGE DETECTION TAB ------------------------------------------------------
+// -------------------------------------------------------------------------------------------------------------------------------------
 
 
 void MainWindow::on_PrewittButton_clicked()
 {
-    if(checkImage()) return;
+    if(checkImage(inputImage)) return;
 
 
 }
 
 
-
-
 void MainWindow::on_RobetButton_clicked()
 {
-    if(checkImage()) return;
-
+    if(checkImage(inputImage)) return;
     Convert_To_Gray(inputMat, edgeDetectionOutputMat);
     edgeDetectionOutputMat = Robert_Edge(edgeDetectionOutputMat);
-    ui->EdgeDetection_outputImage->setPixmap(QPixmap::fromImage(QImage(edgeDetectionOutputMat.data, edgeDetectionOutputMat.cols, edgeDetectionOutputMat.rows, edgeDetectionOutputMat.step, QImage::Format_Grayscale8)));
+    updateImage(edgeDetectionOutputMat, ui->EdgeDetection_outputImage, 0);
 }
 
 
 void MainWindow::on_SobelButton_clicked()
 {
-    if(checkImage()) return;
-
+    if(checkImage(inputImage)) return;
     Convert_To_Gray(inputMat, edgeDetectionOutputMat);
     edgeDetectionOutputMat = Detect_Edges_Sobel(edgeDetectionOutputMat);
-    ui->EdgeDetection_outputImage->setPixmap(QPixmap::fromImage(QImage(edgeDetectionOutputMat.data, edgeDetectionOutputMat.cols, edgeDetectionOutputMat.rows, edgeDetectionOutputMat.step, QImage::Format_Grayscale8)));
+    updateImage(edgeDetectionOutputMat, ui->EdgeDetection_outputImage, 0);
 }
 
 
 void MainWindow::on_CannyButton_clicked()
 {
-    if(checkImage()) return;
-
+    if(checkImage(inputImage)) return;
     Convert_To_Gray(inputMat, edgeDetectionOutputMat);
 }
 
 
+
+
+
+
+
+// ----------------------------------------------------------- THRESHOLD TAB ------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------------------
+
 void MainWindow::on_LocalThresholdButton_clicked()
 {
-    if(checkImage()) return;
+    if(checkImage(inputImage)) return;
 
     Convert_To_Gray(inputMat, thresholdOutputMat);
     local_adaptive_threshold(thresholdOutputMat, thresholdOutputMat);
-    ui->Threshold_OutputImage->setPixmap(QPixmap::fromImage(QImage(thresholdOutputMat.data, thresholdOutputMat.cols, thresholdOutputMat.rows, thresholdOutputMat.step, QImage::Format_Grayscale8)));
+    updateImage(thresholdOutputMat, ui->Threshold_OutputImage, 0);
 }
 
 
 void MainWindow::on_GlobalThresholdButton_clicked()
 {
-    if(checkImage()) return;
+    if(checkImage(inputImage)) return;
 
     Convert_To_Gray(inputMat, thresholdOutputMat);
 
@@ -221,13 +215,139 @@ void MainWindow::on_GlobalThresholdButton_clicked()
     if(thresholdInput.flag) return;
 
     global_threshold(thresholdOutputMat, thresholdOutputMat, thresholdInput.ThresholdValue);
-    ui->Threshold_OutputImage->setPixmap(QPixmap::fromImage(QImage(thresholdOutputMat.data, thresholdOutputMat.cols, thresholdOutputMat.rows, thresholdOutputMat.step, QImage::Format_Grayscale8)));
+    updateImage(thresholdOutputMat, ui->Threshold_OutputImage, 0);
 }
 
-bool MainWindow::checkImage(){
-    if(inputImage.isNull()){
+
+
+
+
+// ----------------------------------------------------------- HYBRID FILTER TAB ------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------------------------------
+
+void MainWindow::on_UploadeImage1_clicked()
+{
+    UploadImage(hybridImage1, hybridImage1Mat);
+    updateFrequencyResponse(hybridImage1Mat, freqImage1Mat, ui->freqOutputImage1, freqImage1Slider, image1_H_L);
+    updateImage(hybridImage1Mat, ui->hybridInputImage1, 1);
+}
+
+
+void MainWindow::on_UploadeImage2_clicked()
+{
+    UploadImage(hybridImage2, hybridImage2Mat);
+    updateFrequencyResponse(hybridImage2Mat, freqImage2Mat, ui->freqOutputImage2, freqImage2Slider, image2_H_L);
+    updateImage(hybridImage2Mat,  ui->hybridInputImage2, 1);
+}
+
+void MainWindow::on_Image1FSlider_valueChanged(int value)
+{
+    image1_H_L = value ^ 1;
+    ui->Image2FSlider->setSliderPosition(image1_H_L);
+}
+
+
+void MainWindow::on_Image2FSlider_valueChanged(int value)
+{
+    image2_H_L = value ^ 1;
+    ui->Image1FSlider->setSliderPosition(image2_H_L);
+}
+
+void MainWindow::on_freqOutputImage1Slider_valueChanged(int value)
+{
+    freqImage1Slider = value;
+    updateFrequencyResponse(hybridImage1Mat, freqImage1Mat, ui->freqOutputImage1, freqImage1Slider, image1_H_L);
+
+}
+
+void MainWindow::on_freqOutputImage2Slider_valueChanged(int value)
+{
+    freqImage2Slider = value;
+    updateFrequencyResponse(hybridImage2Mat, freqImage2Mat, ui->freqOutputImage2, freqImage2Slider, image2_H_L);
+}
+
+
+void MainWindow::on_HybridButton_clicked()
+{
+    finalHybridImageMat =  Apply_Hybrid_Images(freqImage1Mat, freqImage2Mat);
+    updateImage(finalHybridImageMat,  ui->finalHybridImage, 0);
+}
+
+// ----------------------------------------------------------- HELPER FUNCTIONS ------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------------------------------
+
+bool MainWindow::checkImage(QImage &image)
+{
+    if(image.isNull()){
         QMessageBox::information(this, "Image not uploaded", "Please upload image first!");
         return true;
     }
     else return false;
 }
+
+
+void MainWindow::UploadImage(QImage &image, Mat &imageMat)
+{
+    reader.setFileName(QFileDialog::getOpenFileName(this,tr("Open image")));
+    image = reader.read();
+
+    if(!image.isNull()){
+    ui-> BrowseButton -> setText("Updated");
+    ui-> BrowseButton -> setStyleSheet("QPushButton{border-radius: 10px; text-align: left; font: 900 12pt 'Segoe UI Black'; color: green} QPushButton:hover:!pressed{background-color: qlineargradient(x1: 0.5, y1: 1, x2: 0.5, y2: 1, stop: 0 #dadbde, stop: 1 #f6f7fa)}");
+    ui-> BrowseButton -> setDisabled(true);
+    ui-> pushButton-> setDisabled(false);
+    }
+
+    image = image.convertToFormat(QImage::Format_BGR888);
+    imageMat = Mat(image.height(), image.width(), CV_8UC3, image.bits(), image.bytesPerLine());
+
+
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui-> BrowseButton -> setText("Update Image");
+    ui-> BrowseButton -> setStyleSheet("QPushButton{border-radius: 10px; text-align: left; font: 900 12pt 'Segoe UI Black';} QPushButton:hover:!pressed{background-color: qlineargradient(x1: 0.5, y1: 1, x2: 0.5, y2: 1, stop: 0 #dadbde, stop: 1 #f6f7fa)}");
+    ui->BrowseButton->setEnabled(true);
+    ui->pushButton->setDisabled(true);
+    ui->filter_inputImage->clear();
+    ui->filter_outputImage->clear();
+    ui->EdgeDetection_inputImage->clear();
+    ui->EdgeDetection_outputImage->clear();
+    ui->Threshold_InputImage->clear();
+    ui->Threshold_OutputImage->clear();
+
+}
+
+void MainWindow::updateImage(Mat &inputMat,  QLabel* image, bool rgb_flag){
+
+    if(rgb_flag){
+        image->setPixmap(QPixmap::fromImage(QImage(inputMat.data, inputMat.cols, inputMat.rows, inputMat.step, QImage::Format_BGR888)));
+    }
+    else{
+        image->setPixmap(QPixmap::fromImage(QImage(inputMat.data, inputMat.cols, inputMat.rows, inputMat.step, QImage::Format_Grayscale8)));
+    }
+}
+
+
+void MainWindow::updateFrequencyResponse(Mat &inputMat, Mat &freqMat, QLabel* image, int sliderValue, int high_low_flag){
+    Convert_To_Gray(inputMat, freqMat);
+    freqMat = Add_Low_High_Frequency_Filter(freqMat, sliderValue, high_low_flag);
+    image->setPixmap(QPixmap::fromImage(QImage(freqMat.data, freqMat.cols, freqMat.rows, freqMat.step, QImage::Format_Grayscale8)));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- QT_GUI_TASK_1/mainwindow.h
+++ QT_GUI_TASK_1/mainwindow.h
@@ -7,6 +7,14 @@
 #include <uniformnoiseparameters.h>
 #include <thresholdwindow.h>
 
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include "opencv2/core/core.hpp"
+#include <QLabel>
+#include <QImageReader>
+
+using namespace cv;
 
 namespace Ui {
 class MainWindow;
@@ -20,7 +28,39 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+    QImageReader reader;
+
+// ------------ Filer/Noise/Threshold Output Images ----------------
+    QImage inputImage;
+    Mat inputMat = Mat::zeros(1, 1, CV_64F);
+
+    Mat filterOutputMat = Mat::zeros(1, 1, CV_64F);
+    Mat edgeDetectionOutputMat = Mat::zeros(1, 1, CV_64F);
+    Mat thresholdOutputMat = Mat::zeros(1, 1, CV_64F);
+
+// ------------- Hybrid variables ----------------------------------
+
+    QImage hybridImage1;
+    Mat hybridImage1Mat = Mat::zeros(1, 1, CV_64F);
+    Mat freqImage1Mat = Mat::zeros(1, 1, CV_64F);
+    int freqImage1Slider;
+    bool image1_H_L;
+
+
+    QImage hybridImage2;
+    Mat hybridImage2Mat = Mat::zeros(1, 1, CV_64F);
+    Mat freqImage2Mat = Mat::zeros(1, 1, CV_64F);
+    int freqImage2Slider;
+    bool image2_H_L;
+
+    Mat finalHybridImageMat = Mat::zeros(1, 1, CV_64F);
+
+
+
+
+
 private slots:
+
     void on_BrowseButton_clicked();
 
     void on_MedianFilterButton_clicked();
@@ -35,8 +75,6 @@ private slots:
 
     void on_GaussianNoiseButton_clicked();
 
-    bool checkImage();
-
     void on_UniformNoiseButton_clicked();
 
     void on_PrewittButton_clicked();
@@ -50,6 +88,32 @@ private slots:
     void on_LocalThresholdButton_clicked();
 
     void on_GlobalThresholdButton_clicked();
+
+// ----------------------- Hybrid Tab -------------------
+    void on_Image1FSlider_valueChanged(int value);
+
+    void on_Image2FSlider_valueChanged(int value);
+
+    void on_UploadeImage1_clicked();
+
+    void on_UploadeImage2_clicked();
+
+    void on_freqOutputImage1Slider_valueChanged(int value);
+
+    void on_freqOutputImage2Slider_valueChanged(int value);
+
+    void updateFrequencyResponse(Mat &inputMat, Mat &freqMat, QLabel* imageLabel, int sliderValue, int high_low_flag);
+
+// --------------------- Helper Functions ----------------
+    bool checkImage(QImage &image);
+
+    void UploadImage(QImage &image, Mat &imageMat);
+
+    void updateImage(Mat &inputMat,  QLabel* image, bool rgb_flag);
+
+    void on_HybridButton_clicked();
+
+    void on_pushButton_clicked();
 
 private:
     Ui::MainWindow *ui;
